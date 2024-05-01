a56331b55d8ca77fe252bfce3ad91af127005a0c
Tapu chandra bormon <topuhalder627@gmail.com>
2024-05-01 22:12:46 +0600

    test commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const ejs = require('ejs')
+const path = require('path');
+
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+//app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const PORT = 3000 || process.env.PORT
+
+
+app.get("/", function(req, res) {
+    res.render('index')
+})
+
+app.get("/feature", function(req, res) {
+    res.render('feature')
+})
+
+app.get("/reviews", function(req, res) {
+    res.render('reviews')
+})
+
+
+app.get('*', (req, res) => {
+    res.render('Error', {
+        
+    })
+})
+
+
+app.listen(PORT, function(){
+    console.log("Server is running on port "+ PORT)
+})
---
+++ public/js/index.js
@@ -0,0 +1,67 @@
+//sticky nav bar
+let loc = window.location;
+
+if(loc.pathname !== '/'){
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.add('scrolled');
+}
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    // const toggler = document.querySelector('.navbar-toggler');
+    const isMobile = window.matchMedia('(max-width: 768px)').matches;
+    const scrollPosition = window.scrollY;
+    let currentLocation = window.location;
+
+    if (currentLocation.pathname == '/' && !isMobile) {
+        if (scrollPosition >= 100) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    } else if(currentLocation.pathname == '/' && isMobile) {
+        //navbar.classList.add('scrolled');
+        if (scrollPosition >= 10) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+        //navbar.classList.add('navbar-toggler-mobile');
+    }
+    
+    // if (scrollPosition >= 100 && currentLocation.pathname == '/') {
+    //     navbar.classList.add('scrolled');
+    // } else {
+    //     navbar.classList.remove('scrolled');
+    // }
+});
+
+
+
+// image modal
+const images = document.querySelectorAll('.image-wraper img')
+const popupImage = document.querySelector('.popup-img')
+const popup = document.querySelector('.popup-img img')
+    images.forEach(image => {
+        image.onclick = () => {
+            popupImage.style.display = 'block'
+            popup.src = image.getAttribute('src')   
+        }
+})
+
+document.querySelector('.popup-img span').onclick = () => {
+    popupImage.style.display = 'none'
+    navbar.classList.add('scrolled');
+}
+
+
+
+// redirect to the link while click on the menu bar link
+function closeOffcanvas(event) {
+    event.preventDefault(); // Prevent the default behavior of the anchor element
+    var href = event.target.getAttribute('href'); // Get the href attribute of the clicked link
+    setTimeout(function() {
+      window.location.href = href; // Navigate to the link's destination after a short delay
+    }, 300); // Adjust the delay time as needed
+  }
+  
